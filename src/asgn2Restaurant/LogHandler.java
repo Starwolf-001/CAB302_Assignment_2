ea39750ff1ec5d88ab29208fa56c0d2673429a09
Starwolf-001 <the.Starwolf.001@gmail.com>
2017-05-30 16:33:01 +1000

    Added Matthew Pike's name to files he is the author of. Preparing LogHandlerCustomerTests and RestaurantCustomerTests

--- src/asgn2Restaurant/LogHandler.java
+++ src/asgn2Restaurant/LogHandler.java
@@ -20,7 +20,7 @@ import asgn2Pizzas.PizzaFactory;
  * and Customer object - either as an individual Pizza/Customer object or as an
  * ArrayList of Pizza/Customer objects.
  * 
- * @author Person A and Person B
+ * @author Matthew Pike
  *
  */
 public class LogHandler {
--- src/asgn2Tests/LogHandlerCustomerTests.java
+++ src/asgn2Tests/LogHandlerCustomerTests.java
@@ -3,7 +3,8 @@ package asgn2Tests;
 /**
  * A class that tests the methods relating to the creation of Customer objects in the asgn2Restaurant.LogHander class.
  *
- * @author Person A
+ *  * TODO To be updated with LogHandlerCustomerTestsNew content
+ * 		After which LogHandlerCustomerTestsNew should be deleted
  */
 public class LogHandlerCustomerTests {
 	// TO DO
