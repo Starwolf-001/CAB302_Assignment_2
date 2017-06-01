886941670af96ab7616c74cc3a4bdeaaaa36bd6a
Starwolf-001 <the.Starwolf.001@gmail.com>
2017-06-01 22:49:00 +1000

    Actually added RestaurantPizzaTestsNew.

---
+++ src/asgn2Tests/RestaurantPizzaTestsNew.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package asgn2Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Matthew Pike
+ */
+public class RestaurantPizzaTestsNew {
+
+	/**
+	 * Test method for 
+	 * {@link asgn2Restaurant.PizzaRestaurant#PizzaRestaurant()}.
+	 * 
+	 */
+	@Test
+	public void testPizzaRestaurant() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for 
+	 * {@link asgn2Restaurant.PizzaRestaurant#processLog(java.lang.String)}.
+	 * 
+	 */
+	@Test
+	public void testProcessLog() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for 
+	 * {@link asgn2Restaurant.PizzaRestaurant#getPizzaByIndex(int)}.
+	 * 
+	 */
+	@Test
+	public void testGetPizzaByIndex() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for 
+	 * {@link asgn2Restaurant.PizzaRestaurant#getNumPizzaOrders()}.
+	 * 
+	 */
+	@Test
+	public void testGetNumPizzaOrders() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for 
+	 * {@link asgn2Restaurant.PizzaRestaurant#getTotalProfit()}.
+	 * 
+	 */
+	@Test
+	public void testGetTotalProfit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for 
+	 * {@link asgn2Restaurant.PizzaRestaurant#resetDetails()}.
+	 * 
+	 */
+	@Test
+	public void testResetDetails() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
